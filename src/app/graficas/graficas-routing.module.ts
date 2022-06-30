c8059b989ac7818a3652528a73e0fd4000bba261
johan.ramirez <johan.ramirez@sofka.com.co>
2022-06-30 09:55:58 -0500

    Add: lazy loading routes

--- src/app/graficas/graficas-routing.module.ts
+++ src/app/graficas/graficas-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
+import { DonaComponent } from './pages/dona/dona.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'barra',
+        component: BarrasComponent,
+      },
+      {
+        path: 'barras-doble',
+        component: BarrasDobleComponent,
+      },
+      {
+        path: 'dona',
+        component: DonaComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'barra',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class GraficasRoutingModule { }
+export class GraficasRoutingModule {}
