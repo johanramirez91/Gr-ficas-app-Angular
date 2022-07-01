787c263b98fc597bec551a741e5a27025562f304
johan.ramirez <johan.ramirez@sofka.com.co>
2022-06-30 21:21:56 -0500

    Add: bar graphics

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ChartData, ChartEvent, ChartType, ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-grafica-barra',
   templateUrl: './grafica-barra.component.html',
-  styleUrls: ['./grafica-barra.component.scss']
+  styleUrls: ['./grafica-barra.component.scss'],
 })
 export class GraficaBarraComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() horizontal: boolean = false;
 
-  constructor() { }
+  @Input() barChartData!: ChartData;
+  // {
+  //   // labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+  //   // datasets: [
+  //   //   {
+  //   //     data: [65, 59, 80, 81, 56, 55, 40],
+  //   //     label: 'Series A',
+  //   //     backgroundColor: '#FFC107',
+  //   //     hoverBackgroundColor: '#FFF120',
+  //   //   },
+  //   //   {
+  //   //     data: [28, 48, 40, 19, 86, 27, 90],
+  //   //     label: 'Series B',
+  //   //     backgroundColor: '#03A9F4',
+  //   //     hoverBackgroundColor: '#00DDFB',
+  //   //   },
+  //   //   {
+  //   //     data: [35, 65, 25, 45, 86, 95, 90],
+  //   //     label: 'Series C',
+  //   //     backgroundColor: '#4CAF50',
+  //   //     hoverBackgroundColor: '#05E334',
+  //   //   },
+  //   // ],
+  // };
 
-  ngOnInit(): void {
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    indexAxis: 'x',
+  };
+  public barChartType: ChartType = 'bar';
+
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartOptions!.indexAxis = 'y';
+    }
+  }
 }
--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -1,14 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   templateUrl: './barras.component.html',
-  styleUrls: ['./barras.component.scss']
+  styleUrls: ['./barras.component.scss'],
 })
 export class BarrasComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
-  constructor() { }
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
 
-  ngOnInit(): void {
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event?: ChartEvent;
+    active?: {}[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      {
+        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Series A',
+        backgroundColor: '#FFC107',
+        hoverBackgroundColor: '#FFF120',
+      },
+      {
+        data: [28, 48, 40, 19, 86, 27, 90],
+        label: 'Series B',
+        backgroundColor: '#03A9F4',
+        hoverBackgroundColor: '#00DDFB',
+      },
+      {
+        data: [35, 65, 25, 45, 86, 95, 90],
+        label: 'Series C',
+        backgroundColor: '#4CAF50',
+        hoverBackgroundColor: '#05E334',
+      },
+    ],
+  };
+
+  constructor() {}
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      Math.round(Math.random() * 100),
+      56,
+      Math.round(Math.random() * 100),
+      40,
+    ];
+
+    this.chart?.update();
+  }
+
+  ngOnInit(): void {}
 }
